ef9500898411342352035f6e048f0edf12cdb44d
Clownacy <Clownacy@users.noreply.github.com>
2017-02-03 22:23:25 +0000

    Added SDL2 controller input mod

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 CC = gcc
 CFLAGS = -O3 -s -shared
-LIBS = -Isrc/Common
+LIBS = -Isrc/common
 
-all: bin/mod_loader.dll bin/mods/60fps.dll bin/mods/ogg_music_wiiware.dll bin/mods/ogg_music_3d.dll bin/mods/wasd_input.dll
+all: bin/mod_loader.dll bin/mods/60fps.dll bin/mods/ogg_music_wiiware.dll bin/mods/ogg_music_3d.dll bin/mods/sdl_controller_input.dll bin/mods/wasd_input.dll
 
 bin/mod_loader.dll: src/mod_loader/main.c
 	$(CC) $(CFLAGS) -o $@ $^ $(LIBS)
@@ -15,6 +15,9 @@ bin/mods/ogg_music_wiiware.dll: src/example_mods/ogg_music/main.c
 
 bin/mods/ogg_music_3d.dll: src/example_mods/ogg_music/main.c
 	$(CC) $(CFLAGS) -DSOUNDTRACK_3D -o $@ $^ $(LIBS) -lSDL2_mixer -lSDL2
+
+bin/mods/sdl_controller_input.dll: src/example_mods/sdl_controller_input/main.c
+	$(CC) $(CFLAGS) -o $@ $^ $(LIBS) -lSDL2
 
 bin/mods/wasd_input.dll: src/example_mods/wasd_input/main.c
 	$(CC) $(CFLAGS) -o $@ $^ $(LIBS)
---
+++ src/example_mods/sdl_controller_input/main.c
@@ -0,0 +1,162 @@
+#include <windows.h>
+
+#include "SDL2/SDL.h"
+
+#include "mod_loader.h"
+
+#define INPUT_QUIT		0x00008000
+#define INPUT_MAP		0x00000010
+#define INPUT_LEFT		0x00000001
+#define INPUT_RIGHT		0x00000002
+#define INPUT_UP		0x00000004
+#define INPUT_DOWN		0x00000008
+#define INPUT_SHOOT		0x00000020
+#define INPUT_JUMP		0x00000040
+#define INPUT_NEXTWEAPON	0x00000080
+#define INPUT_PREVIOUSWEAPON	0x00000100
+#define INPUT_UNKNOWN		0x00000200
+#define INPUT_F1		0x00000400
+#define INPUT_F2		0x00000800
+#define INPUT_INVENTORY		0x00001000
+#define INPUT_ALT_LEFT		0x00010000
+#define INPUT_ALT_DOWN		0x00020000
+#define INPUT_ALT_RIGHT		0x00040000
+#define INPUT_ALT_UP		0x00080000
+#define INPUT_ALT_UP2		0x00100000
+
+int* const InputBitfield = (int* const)0x49E210;
+
+SDL_Event event;
+SDL_GameController *controller = NULL;
+int controller_id;
+
+void DoButton(const int button_id)
+{
+	if (event.cbutton.type == SDL_CONTROLLERBUTTONDOWN)
+		*InputBitfield |= button_id;
+	else
+		*InputBitfield &= ~button_id;
+}
+
+__cdecl void ProcessControllerEvents(void)
+{
+	while (SDL_PollEvent(&event) != 0)
+	{
+		switch(event.type)
+		{
+			case SDL_CONTROLLERDEVICEADDED:
+			{
+				if (controller == NULL && SDL_IsGameController(event.cdevice.which))
+				{
+					controller = SDL_GameControllerOpen(event.cdevice.which);
+					controller_id = event.cdevice.which;
+				}
+				break;
+			}
+			case SDL_CONTROLLERDEVICEREMOVED:
+			{
+				if (controller != NULL && controller_id == event.cdevice.which)
+				{
+					SDL_GameControllerClose(controller);
+					controller = NULL;
+				}
+				break;
+			}
+			case SDL_CONTROLLERBUTTONDOWN:
+			case SDL_CONTROLLERBUTTONUP:
+			{
+				switch (event.cbutton.button)
+				{
+					case SDL_CONTROLLER_BUTTON_A:
+					{
+						DoButton(INPUT_JUMP);
+						break;
+					}
+					case SDL_CONTROLLER_BUTTON_B:
+					{
+						DoButton(INPUT_NEXTWEAPON);
+						break;
+					}
+					case SDL_CONTROLLER_BUTTON_X:
+					{
+						DoButton(INPUT_SHOOT);
+						break;
+					}
+					case SDL_CONTROLLER_BUTTON_Y:
+					{
+						DoButton(INPUT_PREVIOUSWEAPON);
+						break;
+					}
+					case SDL_CONTROLLER_BUTTON_BACK:
+					{
+						DoButton(INPUT_MAP);
+						break;
+					}
+					case SDL_CONTROLLER_BUTTON_START:
+					{
+						DoButton(INPUT_INVENTORY);
+						break;
+					}
+					case SDL_CONTROLLER_BUTTON_LEFTSHOULDER:
+					{
+						DoButton(INPUT_PREVIOUSWEAPON);
+						break;
+					}
+					case SDL_CONTROLLER_BUTTON_RIGHTSHOULDER:
+					{
+						DoButton(INPUT_NEXTWEAPON);
+						break;
+					}
+					case SDL_CONTROLLER_BUTTON_DPAD_UP:
+					{
+						DoButton(INPUT_UP);
+						break;
+					}
+					case SDL_CONTROLLER_BUTTON_DPAD_DOWN:
+					{
+						DoButton(INPUT_DOWN);
+						break;
+					}
+					case SDL_CONTROLLER_BUTTON_DPAD_LEFT:
+					{
+						DoButton(INPUT_LEFT);
+						break;
+					}
+					case SDL_CONTROLLER_BUTTON_DPAD_RIGHT:
+					{
+						DoButton(INPUT_RIGHT);
+						break;
+					}
+				}
+				break;
+			}
+		}
+	}
+}
+
+__declspec(dllexport) void InitMod(HMODULE mod_loader_hmodule)
+{
+	GetModLoaderFunctions(mod_loader_hmodule);
+
+	SDL_Init(SDL_INIT_GAMECONTROLLER);
+
+	// Grab all controllers that were plugged-in before the game was started
+	for (int i=0; i < SDL_NumJoysticks(); ++i)
+	{
+		if (SDL_IsGameController(i))
+		{
+			controller = SDL_GameControllerOpen(i);
+			controller_id = i;
+			break;
+		}
+	}
+
+	// NOP-out call to DirectInput init function
+	WriteByte(0x420EF6, 0x90);
+	WriteByte(0x420EF6 + 1, 0x90);
+	WriteByte(0x420EF6 + 2, 0x90);
+	WriteByte(0x420EF6 + 3, 0x90);
+	WriteByte(0x420EF6 + 4, 0x90);
+	// Redirect controller update function call
+	WriteRelativeAddress(0x4135CC + 1, ProcessControllerEvents);
+}
