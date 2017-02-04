2195684feeef0f4f9c22328c29e7f10706587010
Clownacy <Clownacy@users.noreply.github.com>
2017-02-04 02:01:55 +0000

    Typo

--- src/example_mods/debug_save/main.c
+++ src/example_mods/debug_save/main.c
@@ -5,7 +5,7 @@
 __declspec(dllexport) void InitMod(HMODULE mod_loader_hmodule)
 {
 	GetModLoaderFunctions(mod_loader_hmodule);
-	// Stop Debug Save menu from being delted
+	// Stop Debug Save menu from being deleted
 	WriteByte(0x412D4D, 0xEB);
 	WriteByte(0x412D4D + 1, 0x0F);
 }
