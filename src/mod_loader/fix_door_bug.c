7dc9b42e7378e5433028d6cf1b77ff74b5e0c0d3
Clownacy <Clownacy@users.noreply.github.com>
2017-02-06 20:32:08 +0000

    Door fix cleanup

--- src/mod_loader/fix_door_bug.c
+++ src/mod_loader/fix_door_bug.c
@@ -8,17 +8,12 @@ const int* const input_bitfield_newly_pressed = (void*)0x49E214;
 const int NORM_MASK = ~(INPUT_LEFT | INPUT_RIGHT | INPUT_DOWN | INPUT_UP);
 const int ALT_MASK = ~(INPUT_ALT_LEFT | INPUT_ALT_RIGHT | INPUT_ALT_DOWN | INPUT_ALT_UP | INPUT_ALT_UP2);
 
-__stdcall const int TestOnlyDownPressedAndHeld(void)
+__stdcall const int TestOnlyDownPressed(void)
 {
-	// What this monster does is check for if only the down key is pressed.
-	// The difference this has from the regular code in Cave Story
-	// is that this excludes the alternate directional control keys.
-	// This fixes a bug in the game where holding ',', '.', '/', 'l', or ';',
-	// and then pressing down, will not let you enter a door.
 	const int current_mask = (*input_down == 8) ? ALT_MASK : NORM_MASK;
 	const int current_down_input = (*input_down == 8) ? INPUT_DOWN : INPUT_ALT_DOWN;
 
-	return (((*input_bitfield_newly_pressed & current_mask) == current_down_input) && ((*input_bitfield_held & current_mask) == current_down_input));
+	return ((*input_bitfield_newly_pressed & current_mask) == current_down_input);
 }
 
 __stdcall const int TestOnlyDownHeld(void)
@@ -31,7 +26,10 @@ __stdcall const int TestOnlyDownHeld(void)
 
 __asm(
 "_TestOnlyDownPressedAndHeld_asm:\n"
-"call	_TestOnlyDownPressedAndHeld@0\n"
+"call	_TestOnlyDownPressed@0\n"
+"or	%eax, %eax\n"
+"jz	0x415826\n"
+"call	_TestOnlyDownHeld@0\n"
 "or	%eax, %eax\n"
 "jz	0x415826\n"
 "jmp	0x4157F3\n"
