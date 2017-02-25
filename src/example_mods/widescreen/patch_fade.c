e6ba0255beaa4e24dc33f50e3ba1e2eb6ae2b768
Clownacy <Clownacy@users.noreply.github.com>
2017-02-25 18:13:56 +0000

    Patch ProcessFade rather than just replace it

--- src/example_mods/widescreen/patch_fade.c
+++ src/example_mods/widescreen/patch_fade.c
@@ -6,277 +6,126 @@
 char fade_buffer_bool[SCREEN_WIDTH * 240];
 char fade_buffer_int[SCREEN_WIDTH * 240];
 
-void ProcessFade(void)
-{
-	int *fade_in_or_out = (int*)0x49DB30;
-	int *fade_complete = (int*)0x49DB34;
-	int *fade_counter = (int*)0x49DB38;
-	int *fade_type = (int*)0x49DD94;
-
-	const int tile_width = ScreenWidthToTiles(16);
-
-	if (*fade_in_or_out == 1)
-	{
-		*fade_complete = 0;
-		switch (*fade_type)
-		{
-			case 0:
-			{
-				for (int i=0; i < 15; ++i)
-				{
-					for (int j=0; j < tile_width; ++j)
-					{
-						if ((tile_width - 1) - *fade_counter == j)
-						{
-							fade_buffer_bool[(tile_width * i) + j] = 1;
-						}
-					}
-				}
-				break;
-			}
-			case 2:
-			{
-				for (int i=0; i < 15; ++i)
-				{
-					for (int j=0; j < tile_width; ++j)
-					{
-						if (*fade_counter == j)
-						{
-							fade_buffer_bool[(tile_width * i) + j] = 1;
-						}
-					}
-				}
-				break;
-			}
-			case 1:
-			{
-				for (int i=0; i < 15; ++i)
-				{
-					for (int j=0; j < tile_width; ++j)
-					{
-						if (14 - *fade_counter == i)
-						{
-							fade_buffer_bool[(tile_width * i) + j] = 1;
-						}
-					}
-				}
-				break;
-			}
-			case 3:
-			{
-				for (int i=0; i < 15; ++i)
-				{
-					for (int j=0; j < tile_width; ++j)
-					{
-						if (*fade_counter == i)
-						{
-							fade_buffer_bool[(tile_width * i) + j] = 1;
-						}
-					}
-				}
-				break;
-			}
-			case 4:
-			{
-				for (int i=0; i < 7; ++i)
-				{
-					for (int j=0; j < tile_width / 2; ++j)
-					{
-						if ((tile_width - 1) - *fade_counter == i + j)
-						{
-							fade_buffer_bool[(tile_width * i) + j] = 1;
-						}
-					}
-				}
-				for (int i=0; i < 7; ++i)
-				{
-					for (int j=tile_width / 2; j < tile_width; ++j)
-					{
-						if ((tile_width - 1) - *fade_counter == i + (tile_width - 1) - j)
-						{
-							fade_buffer_bool[(tile_width * i) + j] = 1;
-						}
-					}
-				}
-				for (int i=7; i < 15; ++i)
-				{
-					for (int j=0; j < tile_width / 2; ++j)
-					{
-						if ((tile_width - 1) - *fade_counter == j + 14 - i)
-						{
-							fade_buffer_bool[(tile_width * i) + j] = 1;
-						}
-					}
-				}
-				for (int i=7; i < 15; ++i)
-				{
-					for (int j=tile_width / 2; j < tile_width; ++j)
-					{
-						if ((tile_width - 1) - *fade_counter == 14 - i + (tile_width - 1) - j)
-						{
-							fade_buffer_bool[(tile_width * i) + j] = 1;
-						}
-					}
-				}
-				break;
-			}
-		}
-		for (int i=0; i < 15; ++i)
-		{
-			for (int j=0; j < tile_width; ++j)
-			{
-				if (fade_buffer_int[(tile_width * i) + j] > 0 && fade_buffer_bool[(tile_width * i) + j])
-				{
-					--fade_buffer_int[(tile_width * i) + j];
-				}
-			}
-		}
-		if (++*fade_counter > 15 + tile_width + 1)
-		{
-			*fade_in_or_out = 0;
-		}
-	}
-	else if (*fade_in_or_out == 2)
-	{
-		switch (*fade_type)
-		{
-			case 0:
-			{
-				for (int i=0; i < 15; ++i)
-				{
-					for (int j=0; j < tile_width; ++j)
-					{
-						if ((tile_width - 1) - *fade_counter == j)
-						{
-							fade_buffer_bool[(tile_width * i) + j] = 1;
-						}
-					}
-				}
-				break;
-			}
-			case 2:
-			{
-				for (int i=0; i < 15; ++i)
-				{
-					for (int j=0; j < tile_width; ++j)
-					{
-						if (*fade_counter == j)
-						{
-							fade_buffer_bool[(tile_width * i) + j] = 1;
-						}
-					}
-				}
-				break;
-			}
-			case 1:
-			{
-				for (int i=0; i < 15; ++i)
-				{
-					for (int j=0; j < tile_width; ++j)
-					{
-						if (14 - *fade_counter == i)
-						{
-							fade_buffer_bool[(tile_width * i) + j] = 1;
-						}
-					}
-				}
-				break;
-			}
-			case 3:
-			{
-				for (int i=0; i < 15; ++i)
-				{
-					for (int j=0; j < tile_width; ++j)
-					{
-						if (*fade_counter == i)
-						{
-							fade_buffer_bool[(tile_width * i) + j] = 1;
-						}
-					}
-				}
-				break;
-			}
-			case 4:
-			{
-				for (int i=0; i < 7; ++i)
-				{
-					for (int j=0; j < tile_width / 2; ++j)
-					{
-						if (*fade_counter == i + j)
-						{
-							fade_buffer_bool[(tile_width * i) + j] = 1;
-						}
-					}
-				}
-				for (int i=0; i < 7; ++i)
-				{
-					for (int j=tile_width / 2; j < tile_width; ++j)
-					{
-						if (*fade_counter == i + (tile_width - 1) - j)
-						{
-							fade_buffer_bool[(tile_width * i) + j] = 1;
-						}
-					}
-				}
-				for (int i=7; i < 15; ++i)
-				{
-					for (int j=0; j < tile_width / 2; ++j)
-					{
-						if (*fade_counter == j + 14 - i)
-						{
-							fade_buffer_bool[(tile_width * i) + j] = 1;
-						}
-					}
-				}
-				for (int i=7; i < 15; ++i)
-				{
-					for (int j=tile_width / 2; j < tile_width; ++j)
-					{
-						if (*fade_counter == 14 - i + (tile_width - 1) - j)
-						{
-							fade_buffer_bool[(tile_width * i) + j] = 1;
-						}
-					}
-				}
-				break;
-			}
-		}
-		for (int i=0; i < 15; ++i)
-		{
-			for (int j=0; j < tile_width; ++j)
-			{
-				if (fade_buffer_int[(tile_width * i) + j] < 15 && fade_buffer_bool[(tile_width * i) + j])
-				{
-					++fade_buffer_int[(tile_width * i) + j];
-				}
-			}
-		}
-		if (++*fade_counter > 15 + tile_width + 1)
-		{
-			*fade_complete = 1;
-			*fade_in_or_out = 0;
-		}
-	}
-}
-
 void PatchFade(void)
 {
-	// Redirect ProcessFade
-	WriteJump(0x40DFE0, ProcessFade);
+	const int screen_width_tiles = ScreenWidthToTiles(16);
 	// BeginFadeOut
 	WriteLong(0x40DF25 + 3, (int)fade_buffer_int);
 	WriteLong(0x40DF36 + 3, (int)fade_buffer_bool);
-	WriteByte(0x40DF16 + 3, ScreenWidthToTiles(16));
-	WriteByte(0x40DF1F + 2, ScreenWidthToTiles(16));
-	WriteByte(0x40DF30 + 2, ScreenWidthToTiles(16));
+	WriteByte(0x40DF16 + 3, screen_width_tiles);
+	WriteByte(0x40DF1F + 2, screen_width_tiles);
+	WriteByte(0x40DF30 + 2, screen_width_tiles);
+
 	// BeginFadeIn
 	WriteLong(0x40DFB5 + 3, (int)fade_buffer_int);
 	WriteLong(0x40DFC6 + 3, (int)fade_buffer_bool);
-	WriteByte(0x40DFA6 + 3, ScreenWidthToTiles(16));
-	WriteByte(0x40DFAF + 2, ScreenWidthToTiles(16));
-	WriteByte(0x40DFC0 + 2, ScreenWidthToTiles(16));
+	WriteByte(0x40DFA6 + 3, screen_width_tiles);
+	WriteByte(0x40DFAF + 2, screen_width_tiles);
+	WriteByte(0x40DFC0 + 2, screen_width_tiles);
+
 	// DrawFade
 	WriteLong(0x40E7E9 + 4, (int)fade_buffer_int);
-	WriteByte(0x40E7DA + 3, ScreenWidthToTiles(16));
-	WriteByte(0x40E7E3 + 2, ScreenWidthToTiles(16));
+	WriteByte(0x40E7DA + 3, screen_width_tiles);
+	WriteByte(0x40E7E3 + 2, screen_width_tiles);
+
+	// ProcessFade
+	WriteByte(0x40E3E3 + 3, screen_width_tiles);
+	WriteLong(0x40E3E9 + 1, screen_width_tiles - 1);
+	WriteByte(0x40E3FC + 2, screen_width_tiles);
+	WriteLong(0x40E402 + 3, (int)fade_buffer_bool);
+
+	WriteByte(0x40E43D + 3, screen_width_tiles);
+	WriteByte(0x40E451 + 2, screen_width_tiles);
+	WriteLong(0x40E457 + 3, (int)fade_buffer_bool);
+
+	WriteByte(0x40E492 + 3, screen_width_tiles);
+	WriteByte(0x40E4AB + 2, screen_width_tiles);
+	WriteLong(0x40E4B1 + 3, (int)fade_buffer_bool);
+
+	WriteByte(0x40E4EC + 3, screen_width_tiles);
+	WriteByte(0x40E500 + 2, screen_width_tiles);
+	WriteLong(0x40E506 + 3, (int)fade_buffer_bool);
+
+	WriteByte(0x40E541 + 3, screen_width_tiles / 2);
+	WriteLong(0x40E547 + 1, screen_width_tiles - 1);
+	WriteByte(0x40E55F + 2, screen_width_tiles);
+	WriteLong(0x40E565 + 3, (int)fade_buffer_bool);
+
+	WriteLong(0x40E589 + 3, screen_width_tiles / 2);
+	WriteByte(0x40E59B + 3, screen_width_tiles);
+	WriteLong(0x40E5A1 + 1, screen_width_tiles - 1);
+	WriteLong(0x40E5AC + 1, screen_width_tiles - 1);
+	WriteByte(0x40E5BE + 2, screen_width_tiles);
+	WriteLong(0x40E5C4 + 3, (int)fade_buffer_bool);
+
+	WriteByte(0x40E5FA + 3, screen_width_tiles / 2);
+	WriteLong(0x40E600 + 1, screen_width_tiles - 1);
+	WriteByte(0x40E61D + 2, screen_width_tiles);
+	WriteLong(0x40E623 + 3, (int)fade_buffer_bool);
+
+	WriteLong(0x40E647 + 3, screen_width_tiles / 2);
+	WriteByte(0x40E659 + 3, screen_width_tiles);
+	WriteLong(0x40E65F + 1, screen_width_tiles - 1);
+	WriteLong(0x40E66A + 1, screen_width_tiles - 1);
+	WriteByte(0x40E683 + 2, screen_width_tiles);
+	WriteLong(0x40E689 + 3, (int)fade_buffer_bool);
+
+	WriteByte(0x40E6BF + 3, screen_width_tiles);
+	WriteByte(0x40E6C8 + 2, screen_width_tiles);
+	WriteLong(0x40E6CE + 4, (int)fade_buffer_int);
+	WriteByte(0x40E6DD + 2, screen_width_tiles);
+	WriteLong(0x40E6E3 + 4, (int)fade_buffer_bool);
+	WriteByte(0x40E6F2 + 2, screen_width_tiles);
+	WriteLong(0x40E6F8 + 3, (int)fade_buffer_int);
+	WriteByte(0x40E705 + 2, screen_width_tiles);
+	WriteLong(0x40E70B + 3, (int)fade_buffer_int);
+	WriteByte(0x40E725 + 6, screen_width_tiles + 16);
+
+	WriteByte(0x40E04B + 3, screen_width_tiles);
+	WriteLong(0x40E051 + 1, screen_width_tiles - 1);
+	WriteByte(0x40E064 + 2, screen_width_tiles);
+	WriteLong(0x40E06A + 3, (int)fade_buffer_bool);
+
+	WriteByte(0x40E0A5 + 3, screen_width_tiles);
+	WriteByte(0x40E0B9 + 2, screen_width_tiles);
+	WriteLong(0x40E0BF + 3, (int)fade_buffer_bool);
+
+	WriteByte(0x40E0FA + 3, screen_width_tiles);
+	WriteByte(0x40E113 + 2, screen_width_tiles);
+	WriteLong(0x40E119 + 3, (int)fade_buffer_bool);
+
+	WriteByte(0x40E154 + 3, screen_width_tiles);
+	WriteByte(0x40E168 + 2, screen_width_tiles);
+	WriteLong(0x40E16E + 3, (int)fade_buffer_bool);
+
+	WriteByte(0x40E1A9 + 3, screen_width_tiles / 2);
+	WriteByte(0x40E1C0 + 2, screen_width_tiles);
+	WriteLong(0x40E1C6 + 3, (int)fade_buffer_bool);
+
+	WriteLong(0x40E1EA + 3, screen_width_tiles / 2);
+	WriteByte(0x40E1FC + 3, screen_width_tiles);
+	WriteLong(0x40E202 + 1, screen_width_tiles - 1);
+	WriteByte(0x40E218 + 2, screen_width_tiles);
+	WriteLong(0x40E21E + 3, (int)fade_buffer_bool);
+
+	WriteByte(0x40E254 + 3, screen_width_tiles / 2);
+	WriteByte(0x40E270 + 2, screen_width_tiles);
+	WriteLong(0x40E276 + 3, (int)fade_buffer_bool);
+
+	WriteLong(0x40E29A + 3, screen_width_tiles / 2);
+	WriteByte(0x40E2AC + 3, screen_width_tiles);
+	WriteLong(0x40E2B2 + 1, screen_width_tiles - 1);
+	WriteByte(0x40E2CF + 2, screen_width_tiles);
+	WriteLong(0x40E2D5 + 3, (int)fade_buffer_bool);
+
+	WriteByte(0x40E30B + 3, screen_width_tiles);
+	WriteByte(0x40E314 + 2, screen_width_tiles);
+	WriteLong(0x40E31A + 4, (int)fade_buffer_int);
+	WriteByte(0x40E32A + 2, screen_width_tiles);
+	WriteLong(0x40E330 + 4, (int)fade_buffer_bool);
+	WriteByte(0x40E33F + 2, screen_width_tiles);
+	WriteLong(0x40E345 + 3, (int)fade_buffer_int);
+	WriteByte(0x40E351 + 2, screen_width_tiles);
+	WriteLong(0x40E357 + 3, (int)fade_buffer_int);
+	WriteByte(0x40E36F + 6, screen_width_tiles + 16);
 }
