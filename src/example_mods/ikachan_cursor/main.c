65f6e0a458258bed42a3a56a68c08bef8af16142
Clownacy <Clownacy@users.noreply.github.com>
2017-03-05 00:13:25 +0000

    Cleaned-up Ikachan cursor mod

--- src/example_mods/ikachan_cursor/main.c
+++ src/example_mods/ikachan_cursor/main.c
@@ -1,8 +1,8 @@
 #include "mod_loader_main.h"
 
-const char* const CURSOR_IKA = "CURSOR_IKA";
+const char* const cursor_filename = "CURSOR_IKA";
 
 void InitMod(void)
 {
-	WriteLong(0x41272B + 1, (int)CURSOR_IKA);
+	WriteLong(0x41272B + 1, (int)cursor_filename);
 }
