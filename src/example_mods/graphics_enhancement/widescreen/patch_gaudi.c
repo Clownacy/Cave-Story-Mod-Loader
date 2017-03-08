9483940a9f8dd84ab4dd5ba28ab2dcd7adeeacca
Clownacy <Clownacy@users.noreply.github.com>
2017-03-08 21:20:10 +0000

    Widescreen: Fixed flying Gaudi
    This damn species...

--- src/example_mods/graphics_enhancement/widescreen/patch_gaudi.c
+++ src/example_mods/graphics_enhancement/widescreen/patch_gaudi.c
@@ -11,6 +11,9 @@ void PatchGaudi(void)
 	// Regular Gaudi
 	WriteLong(0x446029 + 1, SCREEN_WIDTH * 0x200);
 	WriteLong(0x44603C + 2, SCREEN_WIDTH * 0x200);
+	// Flying Gaudi
+	WriteLong(0x44671B + 1, SCREEN_WIDTH * 0x200);
+	WriteLong(0x44672E + 2, SCREEN_WIDTH * 0x200);
 	// Armoured Gaudi
 	WriteLong(0x449E6A + 1, SCREEN_WIDTH * 0x200);
 	WriteLong(0x449E7D + 2, SCREEN_WIDTH * 0x200);
