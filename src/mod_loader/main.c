46b3d85f8ca6179a9e0f22eaacfd898039b524d3
Clownacy <Clownacy@users.noreply.github.com>
2017-06-09 19:34:27 +0100

    Corrected weird formatting

--- src/mod_loader/main.c
+++ src/mod_loader/main.c
@@ -14,43 +14,43 @@ HMODULE this_hmodule;
 
 void LoadMod(const char* const filename)
 {
-		PrintDebug("  Loading mod '%s'...\n", filename);
+	PrintDebug("  Loading mod '%s'...\n", filename);
 
-		char mod_folder_relative[5 + strlen(filename) + 1 + 1];
-		sprintf(mod_folder_relative, "mods\\%s\\", filename);
+	char mod_folder_relative[5 + strlen(filename) + 1 + 1];
+	sprintf(mod_folder_relative, "mods\\%s\\", filename);
 
-		size_t mod_folder_length = GetFullPathName(mod_folder_relative, 0, NULL, NULL);
-		char *mod_folder = malloc(mod_folder_length);
-		GetFullPathName(mod_folder_relative, mod_folder_length, mod_folder, NULL);
+	size_t mod_folder_length = GetFullPathName(mod_folder_relative, 0, NULL, NULL);
+	char *mod_folder = malloc(mod_folder_length);
+	GetFullPathName(mod_folder_relative, mod_folder_length, mod_folder, NULL);
 
-		AddToModList(mod_folder);
+	AddToModList(mod_folder);
 
-		char mod_path[strlen(mod_folder) + strlen(filename) + 1];
-		sprintf(mod_path, "%s%s", mod_folder, filename);
+	char mod_path[strlen(mod_folder) + strlen(filename) + 1];
+	sprintf(mod_path, "%s%s", mod_folder, filename);
 
-		// Load mod DLL
-		HMODULE hmodule = LoadLibrary(mod_path);
-		if (hmodule == NULL)
-		{
-			free(mod_folder);
-			LPVOID error_message;
-			FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&error_message, 0, NULL);
-			PrintError("  Could not load mod '%s'\n  Windows reports: %s\n", filename, error_message);
-			LocalFree(error_message);
-			return;
-		}
+	// Load mod DLL
+	HMODULE hmodule = LoadLibrary(mod_path);
+	if (hmodule == NULL)
+	{
+		free(mod_folder);
+		LPVOID error_message;
+		FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&error_message, 0, NULL);
+		PrintError("  Could not load mod '%s'\n  Windows reports: %s\n", filename, error_message);
+		LocalFree(error_message);
+		return;
+	}
 
-		// Get DLL entry point
-		void (*ModEntry)(HMODULE, void*, const char* const) = (void (*)(HMODULE, void*, const char* const))GetProcAddress(hmodule, "ModEntry");
-		if (ModEntry == NULL)
-		{
-			free(mod_folder);
-			PrintError("  Mod '%s' did not contain a valid entry point (\"ModEntry\")\n", filename);
-			return;
-		}
+	// Get DLL entry point
+	void (*ModEntry)(HMODULE, void*, const char* const) = (void (*)(HMODULE, void*, const char* const))GetProcAddress(hmodule, "ModEntry");
+	if (ModEntry == NULL)
+	{
+		free(mod_folder);
+		PrintError("  Mod '%s' did not contain a valid entry point (\"ModEntry\")\n", filename);
+		return;
+	}
 
-		// Run mod
-		ModEntry(this_hmodule, ReadSettings(filename), mod_folder);
+	// Run mod
+	ModEntry(this_hmodule, ReadSettings(filename), mod_folder);
 }
 
 __declspec(dllexport) void init(void)
