a962aae2eccec4a6e2725dea8a034436472e67fd
Clownacy <Clownacy@users.noreply.github.com>
2017-09-12 00:41:59 +0100

    Hopefully fixed Beetle/Basu spawning
    Fixes #11

--- src/example_mods/graphics_enhancement/widescreen/patch_beetle_and_basu.c
+++ src/example_mods/graphics_enhancement/widescreen/patch_beetle_and_basu.c
@@ -10,15 +10,23 @@
 void PatchBeetleAndBasu(void)
 {
 	// Beetle (object 8)
+	WriteLong((void*)0x427F9A + 1, (((SCREEN_WIDTH - 320) / 2) + 16) * 0x200);
+	WriteLong((void*)0x427FB1 + 2, (((SCREEN_WIDTH - 320) / 2) + 16) * 0x200);
 	WriteLong((void*)0x428010 + 2, ((SCREEN_WIDTH / 2) + 96) * 0x200);
 	WriteLong((void*)0x42802E + 2, ((SCREEN_WIDTH / 2) + 96) * 0x200);
 	// Basu (object 58)
+	WriteLong((void*)0x431CF2 + 1, (((SCREEN_WIDTH - 320) / 2) + 16) * 0x200);
+	WriteLong((void*)0x431D09 + 2, (((SCREEN_WIDTH - 320) / 2) + 16) * 0x200);
 	WriteLong((void*)0x431D93 + 1, ((SCREEN_WIDTH / 2) + 96) * 0x200);
 	WriteLong((void*)0x431DAF + 1, ((SCREEN_WIDTH / 2) + 96) * 0x200);
 	// Beetle2 (object 210)
+	WriteLong((void*)0x45049A + 1, (((SCREEN_WIDTH - 320) / 2) + 16) * 0x200);
+	WriteLong((void*)0x4504B1 + 2, (((SCREEN_WIDTH - 320) / 2) + 16) * 0x200);
 	WriteLong((void*)0x450510 + 2, ((SCREEN_WIDTH / 2) + 96) * 0x200);
 	WriteLong((void*)0x45052E + 2, ((SCREEN_WIDTH / 2) + 96) * 0x200);
 	// Basu2 (object 208)
+	WriteLong((void*)0x44FD82 + 1, (((SCREEN_WIDTH - 320) / 2) + 16) * 0x200);
+	WriteLong((void*)0x44FD99 + 2, (((SCREEN_WIDTH - 320) / 2) + 16) * 0x200);
 	WriteLong((void*)0x44FE23 + 1, ((SCREEN_WIDTH / 2) + 96) * 0x200);
 	WriteLong((void*)0x44FE3F + 1, ((SCREEN_WIDTH / 2) + 96) * 0x200);
 }
